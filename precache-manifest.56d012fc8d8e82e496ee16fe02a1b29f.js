4aa415540222d0d6afaaa1fa5c7ff645136a553e
ciusji <bqjimaster@gmail.com>
2022-08-03 19:32:33 +0800

    Updates

---
+++ precache-manifest.56d012fc8d8e82e496ee16fe02a1b29f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9e243232067764b8bf7f1bd5bf4def",
+    "url": "/elixir-web/index.html"
+  },
+  {
+    "revision": "8a2a54e71f66ed4f0b0e",
+    "url": "/elixir-web/static/js/2.b787232c.chunk.js"
+  },
+  {
+    "revision": "14df4b571cf6b904fc37",
+    "url": "/elixir-web/static/js/main.4a468cad.chunk.js"
+  },
+  {
+    "revision": "22ee5ae503d4dad8afb9",
+    "url": "/elixir-web/static/js/runtime~main.329f3bb6.js"
+  },
+  {
+    "revision": "08df71fac0e278f97c90d1d492378e4a",
+    "url": "/elixir-web/static/media/elixirnote-footer.08df71fa.svg"
+  },
+  {
+    "revision": "8f1fc836112215201a8b8738b1080cf8",
+    "url": "/elixir-web/static/media/possible_sheet.8f1fc836.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elixir-web/precache-manifest.86c0903af1414c504a4846d72002f7ca.js"
+  "/elixir-web/precache-manifest.56d012fc8d8e82e496ee16fe02a1b29f.js"
 );
 
 self.addEventListener('message', (event) => {
