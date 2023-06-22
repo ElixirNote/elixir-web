907d12dc31f61543f136217c4ba770c6a42205cc
ciusji <bqjimaster@gmail.com>
2023-06-22 10:04:55 +0800

    Updates

---
+++ precache-manifest.e1a466dd8c3ef21ecd352a1222d9e1f9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b40dcef182020d8db1836e60f03668c",
+    "url": "/elixir-web/index.html"
+  },
+  {
+    "revision": "769ad6fdfe0c908cace3",
+    "url": "/elixir-web/static/js/2.4e5d6f13.chunk.js"
+  },
+  {
+    "revision": "2522dd4794bb91feefed",
+    "url": "/elixir-web/static/js/main.86cb614e.chunk.js"
+  },
+  {
+    "revision": "22ee5ae503d4dad8afb9",
+    "url": "/elixir-web/static/js/runtime~main.329f3bb6.js"
+  },
+  {
+    "revision": "d5f05a39758db2f4eb906079cd1a005f",
+    "url": "/elixir-web/static/media/auora.d5f05a39.webp"
+  },
+  {
+    "revision": "18f125c92a41af42c53e983841f09ad5",
+    "url": "/elixir-web/static/media/bg-actions.18f125c9.svg"
+  },
+  {
+    "revision": "c7d967bab1880dfa9df5be2b01bb22f8",
+    "url": "/elixir-web/static/media/bigdata-ecosystem.c7d967ba.svg"
+  },
+  {
+    "revision": "08df71fac0e278f97c90d1d492378e4a",
+    "url": "/elixir-web/static/media/elixirnote-footer.08df71fa.svg"
+  },
+  {
+    "revision": "fff8067173b8a188bceb2ce167847381",
+    "url": "/elixir-web/static/media/elixirnote.fff80671.svg"
+  },
+  {
+    "revision": "1ba0602354c4bc381cf27058bfb47433",
+    "url": "/elixir-web/static/media/feature-bg.1ba06023.svg"
+  },
+  {
+    "revision": "2d7ee4e2eefe9058af6e401be84e8013",
+    "url": "/elixir-web/static/media/github.2d7ee4e2.svg"
+  },
+  {
+    "revision": "26070ccf56bebd132e9a36d724a5df63",
+    "url": "/elixir-web/static/media/iceberg.26070ccf.webp"
+  },
+  {
+    "revision": "93fea8a660bbbf839d5124c19e257050",
+    "url": "/elixir-web/static/media/icon-career-analytics.93fea8a6.svg"
+  },
+  {
+    "revision": "5d8ffcc454f42aca3dacde8629808dab",
+    "url": "/elixir-web/static/media/icon-career-cloud.5d8ffcc4.svg"
+  },
+  {
+    "revision": "5fccfb9ac1a2241b03bb665669b6ee06",
+    "url": "/elixir-web/static/media/icon-mission-analytical.5fccfb9a.svg"
+  },
+  {
+    "revision": "e90209aba3091f27bd900fa6b2c5da54",
+    "url": "/elixir-web/static/media/icon-mission-database.e90209ab.svg"
+  },
+  {
+    "revision": "7f475cb47a597817388c295549d7aa5a",
+    "url": "/elixir-web/static/media/icon-mission-settings.7f475cb4.svg"
+  },
+  {
+    "revision": "a185bb6eaa07db286d664b05015512ac",
+    "url": "/elixir-web/static/media/icon-service-server.a185bb6e.svg"
+  },
+  {
+    "revision": "92eff47f462a2be610122d45dcbad50a",
+    "url": "/elixir-web/static/media/mountain.92eff47f.webp"
+  }
+]);
